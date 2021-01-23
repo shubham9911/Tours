8088a8f6c2778353349f7f74c731b7ce1d34c7de
shubham9911 <shubh9911am@gmail.com>
2021-01-24 00:01:54 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-
 import './App.css';
+import React, { useState, useEffect} from 'react';
+import Loading from './Loading';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [ tours, setTours] =useState([]);
+  if(loading){
+    return(
+      <main>
+        <Loading/>
+      </main>
+    )
+  }
   return (
     <div className="App">
       
